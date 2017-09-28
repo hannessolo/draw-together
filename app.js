795d789298fdf044341452fba286901cbbe2883c
hannessolo <hanneshertach@gmail.com>
2017-09-28 14:56:58 +0100

    changed index.html name

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'public', 'sketch.html'));
 });
 
 var server = app.listen(3001, () => {
